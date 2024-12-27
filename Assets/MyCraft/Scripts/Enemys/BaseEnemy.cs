9fda8cc729929a4f2719dabb92806fec0f10bba0
TUHUSE0907\tuhuse <20237042-matsuurayusei@hcs.ac.jp>
2024-12-27 21:33:03 +0900

    teki

---
+++ Assets/MyCraft/Scripts/Enemys/BaseEnemy.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public abstract class BaseEnemy : MonoBehaviour
+{
+    //protected EnemyMovement _movement;
+    //protected EnemyJump _jump;
+    //protected AttackBase _attack;
+
+    //public void SetMovement(EnemyMovement move) => _movement = move;
+    //public void SetJump(EnemyJump jmp) => _jump = jmp;
+    //public void SetAttack(AttackBase atk) => _attack = atk;
+
+    //public void Move(Vector3 direction) => _movement?.Move(direction);
+    //public void Jump() => _jump?.PerformJump();
+    //public void Attack() => _attack?.ExecuteAttack();
+}
